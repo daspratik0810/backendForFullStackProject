fce5ed2921ae1b417e626994d5afccfabda6b545
Pratik Das <daspratik0810@gmail.com>
2026-07-21 14:05:03 +0000

    userSchema and models

---
+++ src/models/video.models.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema } from "mongoose"
+
+const videoSchema = new mongoose.Schema(
+    {
+        username : {
+            type : String,
+            required : true,
+            unique : true,
+            lowercase : true,
+            trim : true,
+            index:true
+        },
+        email : {
+            type : String,
+            required : true,
+            unique : true,
+            lowercase : true,
+            trim : true,
+        },
+        fullname : {
+            type : String,
+            required : true,
+            trim : true,
+            index : true
+        },
+        avatar : {
+            type : String,  //use cloudinary url
+            required : true,
+            unique : true,
+            lowercase : true,
+            trim : true,
+        },
+        coverImage : {
+            type : String,  //use cloudinary url
+        }, 
+        watchHistory :  [ 
+            {
+                type : Schema.Types.ObjectId,
+                ref : "Video"
+            }
+        ],
+        password : {
+            type : String,
+            required :  [true,"Password is required !!"]
+        },
+        refreshToken : {
+            type : String,
+        },     
+    }, {timestamps : true}
+)
+
+export const Video = mongoose.model(Video,videoSchema)
