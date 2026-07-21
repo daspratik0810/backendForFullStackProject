f670ad77fa9fad2dcc363de7e80645e272c5dfb3
Pratik Das <daspratik0810@gmail.com>
2026-07-21 11:15:31 +0000

    promise handled at connectDB due to async await

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import mongoose from "mongoose"
 import connectDB from "./db/index.js"
 
 import dotenv from "dotenv";
+import { app } from "./app.js";
 dotenv.config({ path: "./.env" });
 
 connectDB()
+.then( ()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port ${process.env.PORT || 8000}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGODb connection failed !!",err)
+}) 
 
 /*
 //SECOND APPROACH - IIFE - Immediately Invoked Function Expression ()()
