fab9beffee9bf842ed3a5dfbb0502fc9f040f482
Pratik Das <daspratik0810@gmail.com>
2026-07-26 11:30:03 +0000

    asyncHandler comments made better

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,6 @@
 //Async functions always return a promise.
-//the promise is resolved with "resolve" or error is caught by catch
+//alternatevely we can also use try-catch and async-await 
+//here the promise is resolved with "resolve" or error is caught by catch
 const asyncHandler = (requestHandler) => {
     (req,res,next) =>{
         //promise has two parts, a resolve and a reject/catch
