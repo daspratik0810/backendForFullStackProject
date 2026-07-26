bd45e02076cd5f5ae2b7334f123476a2f14e2d89
Pratik Das <daspratik0810@gmail.com>
2026-07-26 12:41:28 +0000

    user.model with bcrypt (hashing passowrds) functionality

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new mongoose.Schema(
     {
@@ -48,5 +50,19 @@ const userSchema = new mongoose.Schema(
         },     
     }, {timestamps : true}
 )
+
+//want to perform the function whenever the data is getting saved, here the function hashes(similar to encryption) the password with 10 hashrounds/salts
+userSchema.pre("save", async function(next) {
+    if( this.isModified("password")) {
+        this.password = bcrypt.hash(this.password, 10)
+        next()
+    }
+    return next()
+})
+
+//custom methods with userSchema
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
 
 export const User = mongoose.model(User,userSchema)
