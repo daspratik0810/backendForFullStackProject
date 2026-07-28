7b28944c1869464cd188bb125d8e0edaae30283e
Pratik Das <daspratik0810@gmail.com>
2026-07-28 14:42:10 +0000

    POSTMAN CONNECTED with confirmation message in terminal

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async (req,res) => {
-    console.log("registerUser called");  //we will see this in terminal if POSTMAN or anyone access it, make sure the visibility is public in PORT tab in the bottom here
+    console.log("registerUser called");  //we will see this message in terminal if POSTMAN or anyone access it, make sure the visibility is public in PORT tab in the bottom here
     res.status(200).json({
         message : "ok"
     })
