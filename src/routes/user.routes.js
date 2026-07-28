261cc8f7aad5a6504e4cfff7cf16b3c28b72c1ef
Pratik Das <daspratik0810@gmail.com>
2026-07-28 14:25:28 +0000

    comment for POST and POSTMAN

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,7 @@ import { registerUser } from "../controllers/user.controller.js";
 
 const router = Router()
 
-
+//this /register can only handle POST request (use POSTMAN)
 router.route("/register").post(registerUser)
 
 export default router
