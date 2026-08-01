09849b997d3a00b416f854b3bb6e93342c7d2dcd
Pratik Das <daspratik0810@gmail.com>
2026-08-01 23:50:42 +0000

    custom authentication middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,21 @@
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+
+export const verifyJWT = asyncHandler(async(req, res, next) => {
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+
+    if(!token){
+        throw new ApiError(401,"Unauthorized request")
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+    const user = await User.findById(decodedToken?._id).select("-password -refresToken")
+
+    if(!user){
+        throw new ApiError(401, "Invalid Access Token")
+    }
+
+    req.user = user
+    next()
+})
