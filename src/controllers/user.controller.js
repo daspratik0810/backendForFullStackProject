bd0d65720cb95ac43c097e42fb07fe0e5a78a9ab
Pratik Das <daspratik0810@gmail.com>
2026-08-02 15:49:49 +0000

    Implement password change and account update functionalities; add current user retrieval

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -207,8 +207,7 @@ const logoutUser = asyncHandler(async(req, res) => {
         .json(new ApiResponse(200, {}, "User logged Out"))
 })
 
-const refreshAccessToken = asyncHandler(async (req, res)
-=> {
+const refreshAccessToken = asyncHandler(async (req, res)=> {
     const incomingRefreshToken= req.cookies.refreshToken || req.body.refreshToken
 
     if(!incomingRefreshToken){
@@ -257,6 +256,51 @@ const refreshAccessToken = asyncHandler(async (req, res)
     
 })
 
+const changeCurrentPassword =asyncHandler(async (req,res) =>{
+    const {oldPassword, newPassword} = req.body
 
-export {registerUser, loginUser, logoutUser,refreshAccessToken}
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400,"Invalid old password")
+    }
+
+    user.password = newPassword
+    await user.save({validateBeforeSave:false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,{},"Password changed successfull}y !!"))
+})
+
+const getCurrentUser = asyncHandler(async(req,res)=>{
+    return res
+    .status(200)
+    .json(200,req.user,"Current user fetched successfully !!")
+})
+
+const updateAccountDetails = asyncHandler(async(req,res)=>{
+    const {fullName, email} =req.body
+
+    if(!fullName || !email){
+        throw new ApiError(400,"All fields are required")
+    }
+    const user = User.findByIdAndUpdate(
+        req.user?._idid,
+        {
+            $set : {
+                fullName,
+                email,
+            }
+        },
+        {new:true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully !!"))
+})
+
+export {registerUser, loginUser, logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails}
 
