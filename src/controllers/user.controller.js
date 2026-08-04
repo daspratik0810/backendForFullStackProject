f68d529a8367ec99e25605b01eb7edb08c6184b2
Pratik Das <daspratik0810@gmail.com>
2026-08-04 01:15:03 +0000

    Add error handling and response for non-existent user channels in getUserChannelProfile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -433,7 +433,13 @@ const getUserChannelProfile = asyncHandler(async(req,res) => {
 
     ])  
 
+    if(!channel?.length){
+        throw new ApiError(404,"Channel does not exists !!")
+    }
 
+    return res
+    .status(200)
+    .json( new ApiResponse(200,channel[0],"User channel fetched successfully !!") )
 })
 
 
