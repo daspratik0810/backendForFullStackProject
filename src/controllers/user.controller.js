873c52ebdf930860d04b401cf0be9fe628475dd1
Pratik Das <daspratik0810@gmail.com>
2026-08-05 13:23:25 +0000

    Add getWatchHistory function to retrieve user's watched videos with owner details using subpipelines concept

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 //it will generate and return the access and refresh token
 const generateAccessAndRefreshTokens = async(userId) =>{
@@ -442,6 +443,52 @@ const getUserChannelProfile = asyncHandler(async(req,res) => {
     .json( new ApiResponse(200,channel[0],"User channel fetched successfully !!") )
 })
 
+const getWatchHistory = asyncHandler(async(req,res) => {
+    const user = await User.aggregate([
+        {
+            $match : {
+                _id : new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup : {
+                from : "videos",
+                localField : "watchHistory",
+                foreignField : "_id",
+                as : "watchHistory",
+                //sub pipeline 1
+                pipeline : [
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField : "owner",
+                            foreignField : "_id",
+                            as : "owner",
+                            //sub pipeline 1.1
+                            pipeline : [
+                                {
+                                    $project : {
+                                        fullName : 1,
+                                        username :1,
+                                        avatar : 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    //sub pipeline 2
+                    {
+                        $addFields : {
+                            owner : {
+                                $first : "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+})
 
-export {registerUser, loginUser, logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile}
+export {registerUser, loginUser, logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory}
 
