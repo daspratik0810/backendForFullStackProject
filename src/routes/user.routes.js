431749e98a4b8f4697aef707375bef737720835d
Pratik Das <daspratik0810@gmail.com>
2026-08-05 13:59:18 +0000

    Refactor user routes for improved code consistency and formatting

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import {  loginUser, logoutUser,  registerUser, refreshAccessToken, changeCurrentPassword,  getCurrentUser, updateUserAvatar,   updateUserCoverImage,  getUserChannelProfile, getWatchHistory} from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
@@ -11,12 +11,12 @@ router.route("/register").post(
     //multer middleware
     upload.fields([
         {
-            name : "avatar",
-            maxCount : 1 
+            name: "avatar",
+            maxCount: 1
         },
         {
             name: "coverImage",
-            maxCount :1
+            maxCount: 1
         }
     ]),
     registerUser
@@ -27,13 +27,13 @@ router.route("/login").post(loginUser)
 //secure routes. After next() of verifyJWT, it will go to logoutUser. Hence logoutUser can only be routed to the users who are verified(loggedin)
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-router.route("/change-password").post(verifyJWT,changeCurrentPassword) // Hence here aswell changeCurrentPassword can only be routed to the users who are verified(loggedin), thats why we used verifyJWT
-router.route("/current-user").get(verifyJWT,getCurrentUser) // Hence here aswell getCurrentUser can only be routed to the users who are verified(loggedin), thats why we used verifyJWT
-router.route("/update-account").patch(verifyJWT,updateAccountDetails) // Hence here aswell updateAccountDetails can only be routed to the users who are verified(loggedin), thats why we used verifyJWT
-router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)// Hence here aswell updateUserAvatar can only be routed to the users who are verified(loggedin), thats why we used verifyJWT, also upload.single("avatar") is used to upload a single file with the name "avatar"
-router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)// Hence here aswell updateUserCoverImage can only be routed to the users who are verified(loggedin), thats why we used verifyJWT, also upload.single("/coverImage") is used to upload a single file with the name "coverImage"
-router.route("/c/:username").get(verifyJWT,getUserChannelProfile) // Hence here aswell getUserChannelProfile can only be routed to the users who are verified(loggedin), thats why we used verifyJWT
-router.route("/history").get(verifyJWT,getWatchHistory) // Hence here aswell getWatchHistory can only be routed to the users who are verified(loggedin), thats why we used verifyJWT
+router.route("/change-password").post(verifyJWT, changeCurrentPassword) // Hence here aswell changeCurrentPassword can only be routed to the users who are verified(loggedin), thats why we used verifyJWT
+router.route("/current-user").get(verifyJWT, getCurrentUser) // Hence here aswell getCurrentUser can only be routed to the users who are verified(loggedin), thats why we used verifyJWT
+router.route("/update-account").patch(verifyJWT, updateAccountDetails) // Hence here aswell updateAccountDetails can only be routed to the users who are verified(loggedin), thats why we used verifyJWT
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)// Hence here aswell updateUserAvatar can only be routed to the users who are verified(loggedin), thats why we used verifyJWT, also upload.single("avatar") is used to upload a single file with the name "avatar"
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)// Hence here aswell updateUserCoverImage can only be routed to the users who are verified(loggedin), thats why we used verifyJWT, also upload.single("/coverImage") is used to upload a single file with the name "coverImage"
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile) // Hence here aswell getUserChannelProfile can only be routed to the users who are verified(loggedin), thats why we used verifyJWT
+router.route("/history").get(verifyJWT, getWatchHistory) // Hence here aswell getWatchHistory can only be routed to the users who are verified(loggedin), thats why we used verifyJWT
 
 
 export default router
