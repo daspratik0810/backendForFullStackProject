faf44e4fea129db1b539644016b4b0449532359e
Pratik Das <daspratik0810@gmail.com>
2026-08-05 13:34:46 +0000

    Add changeCurrentPassword route to user routes with JWT verification

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { refreshAccessToken } from "../controllers/user.controller.js";
+
 const router = Router()
 
 //this /register can only handle POST request (use POSTMAN)
@@ -23,9 +24,10 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser)
 
-//secure routes. After next() of verifyJWT, it will go to logoutUser 
+//secure routes. After next() of verifyJWT, it will go to logoutUser. Hence logoutUser can only be routed to the users who are verified(loggedin)
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-
+router.route("/change-password").post(verifyJWT,changeCurrentPassword) // . Hence here aswell changeCurrentPassword can only be routed to the users who are verified(loggedin), thats why we used verifyJWT
+ 
 
 export default router
